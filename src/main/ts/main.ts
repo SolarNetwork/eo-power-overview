9fc032f0aeadda15c9cec2fbb59da3013e96719c
Matt Magoffin <matt@solarnetwork.net>
2024-06-20 06:22:04 +1200

    Set background color to config value.

--- src/main/ts/main.ts
+++ src/main/ts/main.ts
@@ -7,6 +7,12 @@ import { SeriesConfig, seriesConfig } from "./utils.ts";
 const urlParams = Object.fromEntries(new URLSearchParams(location.search));
 const config: Required<SeriesConfig> = seriesConfig(urlParams);
 
+if (config.backgroundColor != "#FFFFFF") {
+	document
+		.querySelector<HTMLBodyElement>("body")
+		?.style.setProperty("background-color", config.backgroundColor);
+}
+
 snLoadData(config)
 	.then((datum) => {
 		import("./charts.ts").then(({ renderChart, updateChart }) => {
